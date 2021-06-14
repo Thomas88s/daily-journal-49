d9effce07c38354678d237e6b12cce3df4cccbec
Thomas Witherow <twitherow@bellsouth.net>
2021-06-14 15:07:58 -0500

    got all showing

---
+++ scripts/DailyJournal.js
@@ -0,0 +1,11 @@
+
+    import { Entries } from "./Entries.js"
+
+    export const DailyJournal = () => {
+        return `
+            <div class="entryList">
+                ${ Entries() }
+            </div>
+        `
+    }
+    
